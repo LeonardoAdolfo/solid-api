66d9c0f3cf7cff9529634659ed64f3e891daf593
Leonardo Adolfo <adolfoleo76@gmail.com>
2023-12-21 12:09:22 -0300

    reversing dependencies

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,36 +1,30 @@
-import {FastifyRequest, FastifyReply} from 'fastify'
-import { hash } from 'bcryptjs'
-import { prisma } from "@/lib/prisma"
-import { z } from "zod"
+/* eslint-disable prettier/prettier */
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { RegisteruseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
-export async function register(request:FastifyRequest, reply:FastifyReply) {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try{
+    const UsersRepository = new PrismaUsersRepository()
+    const registerUseCase =  new RegisteruseCase(UsersRepository)
+
+    await registerUseCase.execute({
+        name,
+        email,
+        password
     })
 
-    const { name, email, password } = registerBodySchema.parse(request.body)
-
-    const password_hash = await hash(password, 6)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email,
-        }
-    })
-
-    if (userWithSameEmail) {
-        reply.status(409).send()
-    }
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash
-        }
-    })
-
-    return reply.status(201).send()
+  } catch (err) {
+    reply.status(409).send()
+  }
+  return reply.status(201).send()
 }
---
+++ src/use-cases/register.ts
@@ -0,0 +1,40 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prettier/prettier */
+import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaserequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisteruseCase {
+  constructor(private usersRepository: any) {}
+
+  async execute({ name, email, password }: RegisterUseCaserequest) {
+    const password_hash = await hash(password, 6)
+  
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+  
+    if (userWithSameEmail) {
+      throw new Error("E-mail already exists")
+    }
+  
+    // const prismaUsersRepository = new PrismaUsersRepository()
+  
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+  
+}
+
