da2c868cd782e4f1271ce94f95fb52401cd4fa51
Leonardo Adolfo <adolfoleo76@gmail.com>
2023-12-29 11:50:18 -0300

    Creating Factory Pattern

---
+++ src/use-cases/factories/make-authenticate-use-case.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { AuthenticateUseCase } from '../authenticate'
+
+
+export function makeAuthenticateUseCase() {
+    const usersRepository = new PrismaUsersRepository()
+    const authenticateUseCase =  new AuthenticateUseCase(usersRepository)
+  return authenticateUseCase
+}
---
+++ src/use-cases/factories/make-register-use-case.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '../register'
+
+export function makeRegisterUseCase() {
+  const usersRepository = new PrismaUsersRepository()
+  const registerUseCase = new RegisterUseCase(usersRepository)
+
+  return registerUseCase
+}
