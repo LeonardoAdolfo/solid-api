defe00e1e465d403e321942336cac45d2325a260
Leonardo Adolfo <adolfoleo625@gmail.com>
2024-01-29 11:56:39 -0300

    Ading E2E Tests

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,6 +1,23 @@
-import {test} from 'vitest'
+import request from "supertest"
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
 
+describe('Register (e2e)',()=>{
+    beforeAll(async ()=>{
+        await app.ready()
+    })
+    afterAll(async ()=>{
+        await app.close()
+    })
+    it('should be able register', async ()=> {
+        const response = await request(app.server)
+        .post('/users')
+        .send({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456'
+        })
 
-test('ok',()=> {
-
+        expect(response.statusCode).toEqual(201)
+    })
 })
